47baa29c5ff6dff90c791b7956bc73b9fb01db8d
fitri-annisa <fitrinshl@alumni.ui.ac.id>
2024-02-28 00:30:16 +0700

    test

--- nodejs/app.test.js
+++ nodejs/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('should return "Hello, world! This is Fitri from NodeJS App."', async () => {
+  it('should return "Hello, world! This is Fitri."', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, world! This is Fitri from NodeJS App.');
+    expect(response.text).toBe('Hello, world! This is Fitri.');
   });
 });
