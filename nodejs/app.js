030462469d28d371fa3a507b7d81ffef23582968
fitri-annisa <fitrinshl@alumni.ui.ac.id>
2024-02-27 23:35:30 +0700

    sync app update

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world! This is Fitri.');
+  res.send('Hello, world! This is Fitri from NodeJS App.');
 });
 
 app.listen(port, () => {
