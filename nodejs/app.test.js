ff332fd1c10b0f0460539e7bab91bc9a0f90cd1a
fitri-annisa <fitrinshl@alumni.ui.ac.id>
2024-02-27 22:47:21 +0700

    fix value of string on node

--- nodejs/app.test.js
+++ nodejs/app.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('should return "Hello, world! This is Fitri from Node App that deployed on microk8s."', async () => {
+  it('should return "Hello, world! This is Fitri from NodeJS App that deployed on microk8s."', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, world! This is Fitri from Node App that deployed on microk8s.');
+    expect(response.text).toBe('Hello, world! This is Fitri from NodeJS App that deployed on microk8s.');
   });
 });
